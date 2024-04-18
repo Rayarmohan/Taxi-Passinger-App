27475233af778fefd8be50643b34031f1728edf4
Kailas0201 <159515507+kailasacube@users.noreply.github.com>
2024-04-18 10:47:51 +0530

    home page bugs fixed

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -11,89 +11,91 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: TransparentAppBar(
-        title: Text(
-          "Find Your Ride",
-          style: Theme.of(context).textTheme.displayMedium!.copyWith(
-              height: 1.7, color: AppColors.primeryColor, fontSize: 20),
-        ),
-        leading: Padding(
-          padding: const EdgeInsets.only(top: 10, left: 20),
-          child: InkWell(
-            onTap: () {
-              Get.back();
-            },
-            child: const CustomPngImage(
-              imageName: "assets/images/arrow_back.png",
-              height: 50,
-              width: 50,
-              boxFit: BoxFit.contain,
-            ),
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: TransparentAppBar(
+          title: Text(
+            "Find Your Ride",
+            style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                height: 1.7, color: AppColors.primeryColor, fontSize: 20),
           ),
-        ),
-        actions: const [
-
-        ],
-      ),
-      body: Stack(
-        children: [
-          const SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-            child: CustomPngImage(
-              imageName: 'assets/images/map1.png',
-              boxFit: BoxFit.cover,
-            ),
-          ),
-          const Align(
-            alignment: Alignment.topRight,
-            child: Padding(padding: EdgeInsets.only(top: 15, right: 15),
-              child: Column(
-                children: [
-                  Column(
-                    children: [
-                      CustomPngImage(
-                        imageName: "assets/images/call_icon1.png",
-                        height: 40,
-                        width: 40,
-                        boxFit: BoxFit.contain,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10,),
-                  CustomPngImage(
-                              imageName: "assets/images/warning_icon1.png",
-                    height: 40,
-                    width: 40,
-                    boxFit: BoxFit.contain,
-                  ),
-
-                ],
+          leading: Padding(
+            padding: const EdgeInsets.only(top: 10, left: 20),
+            child: InkWell(
+              onTap: () {
+                Get.back();
+              },
+              child: const CustomPngImage(
+                imageName: "assets/images/arrow_back.png",
+                height: 50,
+                width: 50,
+                boxFit: BoxFit.contain,
               ),
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.only(right: 30, bottom: 90),
-            child: Align(
-              alignment: Alignment.center,
+          actions: const [
+      
+          ],
+        ),
+        body: Stack(
+          children: [
+            const SizedBox(
+              width: double.infinity,
+              height: double.infinity,
               child: CustomPngImage(
-                height: 50,
-                width: 50,
-                imageName: 'assets/images/location1.png',
+                imageName: 'assets/images/map1.png',
                 boxFit: BoxFit.cover,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15.0, bottom: 20),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: _listview(),
+            const Align(
+              alignment: Alignment.topRight,
+              child: Padding(padding: EdgeInsets.only(top: 15, right: 15),
+                child: Column(
+                  children: [
+                    Column(
+                      children: [
+                        CustomPngImage(
+                          imageName: "assets/images/call_icon1.png",
+                          height: 40,
+                          width: 40,
+                          boxFit: BoxFit.contain,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+                    CustomPngImage(
+                                imageName: "assets/images/warning_icon1.png",
+                      height: 40,
+                      width: 40,
+                      boxFit: BoxFit.contain,
+                    ),
+      
+                  ],
+                ),
+              ),
             ),
-          ),
-        ],
+            const Padding(
+              padding: EdgeInsets.only(right: 30, bottom: 90),
+              child: Align(
+                alignment: Alignment.center,
+                child: CustomPngImage(
+                  height: 50,
+                  width: 50,
+                  imageName: 'assets/images/location1.png',
+                  boxFit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, bottom: 20),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: _listview(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
